707c311a285ba4482f690b5d7e173fce2be4e74f
devlambertjoao <devlambertjoao@gmail.com>
2025-04-04 11:28:12 -0300

    lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,22 +1,105 @@
-require("plugins.lsp.typescript")
+require("plugins.lsp_servers.typescript")
+require("plugins.lsp_servers.lua")
+
+-- Virtual Text Config
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = "●",
+  },
+  -- virtual_text = false,
+  severity_sort = true,
+  update_in_insert = true,
+  float = {
+    -- source = "always",
+    source = true,
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "e",
+      [vim.diagnostic.severity.WARN] = "w",
+      [vim.diagnostic.severity.HINT] = "h",
+      [vim.diagnostic.severity.INFO] = "i",
+    },
+  },
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    vim.bo[args.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+    -- local client = vim.lsp.get_client_by_id(args.data.client_id)
+    local bufnr = args.buf
+    local opts = { buffer = bufnr, noremap = true, silent = true }
+
+    -- Code navigation
+    vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>gk", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>gK", vim.lsp.buf.signature_help, opts)
+
+    -- Code actions
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>cf", function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
+    -- vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, opts)
+
+    -- Diagnostics
+    vim.keymap.set("n", "<leader>da", "<cmd>Telescope diagnostics<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>dl", vim.diagnostic.setqflist, opts)
+    vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "<leader>dn", function()
+      vim.diagnostic.jump({ count = 1 })
+    end, opts)
+
+    -- Go to previous diagnostic
+    vim.keymap.set("n", "<leader>dp", function()
+      vim.diagnostic.jump({ count = -1 })
+    end, opts)
+  end,
+})
+
+
 
 return {
-	{
-		"williamboman/mason.nvim",
-		event = { "VeryLazy" },
-		config = function()
-			local manson = require("mason")
-			manson.setup({
-				ui = {
-					icons = {
-						package_installed = "+",
-						package_pending = "~",
-						package_uninstalled = "-",
-					},
-				},
-			})
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim"
+    },
+    event = { "VeryLazy" },
+    config = function()
+      local manson = require("mason")
+      manson.setup({
+        ui = {
+          icons = {
+            package_installed = "+",
+            package_pending = "~",
+            package_uninstalled = "-",
+          },
+        },
+      })
+
+      local mason_lspconfig = require("mason-lspconfig")
+      mason_lspconfig.setup({
+        automatic_installation = false,
+        ensure_installed = {
+          "lua_ls", -- Lua
+          "solargraph", -- Ruby
+          -- "rust_analyzer", -- Rust
+          -- "clangd", -- C
+          "jsonls", -- JSON
+          "html", -- HTML
+          "ts_ls", -- Typescript
+          "tailwindcss", -- Tailwindcss
+          -- "pyright", -- Python
+          "bashls", -- Bash
+        },
+      })
+    end,
+  },
 }
 
 -- return {
