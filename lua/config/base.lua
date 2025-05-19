3b171ed4994f11c99d2aa08247c8da64392bcf44
devlambertjoao <devlambertjoao@gmail.com>
2025-05-19 20:28:30 -0300

    update base

--- lua/config/base.lua
+++ lua/config/base.lua
@@ -1,34 +1,50 @@
 vim.cmd([[
-  
+
   set expandtab
   set smartindent
   set tabstop=2
   set shiftwidth=2
-  
+
   set mouse=a
-  
+
   set noshowmode
   set noshowcmd
-  
+
   set relativenumber
   set number
   set signcolumn=yes:1
-  
+
   set encoding=utf-8
   "set fileencoding=utf-8
-  
+
   set pumheight=7
-  
+
   set cursorline
-  
+
   set noswapfile
-  
+  set nowritebackup
+  set nobackup
+
   set updatetime=100
-  
-  " set nowrap
+
+  set nowrap
 
   set background=dark
   set termguicolors
+
+  set autoread
+  set ttyfast
+  set lazyredraw
+
+  " Increase/descrease window split size
+  if bufwinnr(1)
+  	map + <C-W>+
+  	map - <C-W>-
+  endif
+
+  nmap <silent> <space> :noh<CR>
+
+  set syntax=on
 ]])
 
 -- Close quickfix list after jump
