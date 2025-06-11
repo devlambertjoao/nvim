14ec007e997d96e7765567ad42b03297cbec61f0
Joao <devlambertjoao@gmail.com>
2025-06-11 00:30:30 -0300

    Initial configuration

---
+++ lua/core.lua
@@ -0,0 +1,29 @@
+local opt = vim.opt
+
+opt.expandtab = true
+opt.smartindent = true
+opt.tabstop = 2
+opt.shiftwidth = 2
+
+opt.relativenumber = true
+opt.number = true
+opt.signcolumn = 'yes:1'
+
+opt.encoding = 'utf-8'
+
+opt.cursorline = true
+opt.wrap = false
+
+opt.swapfile = false
+opt.writebackup = false
+opt.backup = false
+
+opt.updatetime = 100
+opt.autoread = true
+opt.ttyfast = true
+opt.lazyredraw = true
+
+opt.background = 'dark'
+opt.termguicolors = true
+
+opt.clipboard = "unnamedplus"
