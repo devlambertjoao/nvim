766d248be596b1b53ce753c24e383e5d42aa12f7
devlambertjoao <devlambertjoao@gmail.com>
2025-07-04 00:24:22 -0300

    transparentize

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,27 @@ return {
   priority = 1000,
   lazy = false,
   config = function()
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "*",
+      callback = function()
+        vim.cmd [[
+      hi Normal guibg=NONE ctermbg=NONE
+      hi NormalNC guibg=NONE ctermbg=NONE
+      hi NvimTreeNormal guibg=NONE ctermbg=NONE
+      hi SignColumn guibg=NONE ctermbg=NONE
+      hi VertSplit guibg=NONE ctermbg=NONE
+      hi EndOfBuffer guibg=NONE ctermbg=NONE
+      hi NormalFloat guibg=NONE
+      hi FloatBorder guibg=NONE
+      hi TelescopeBorder guibg=NONE
+      hi Pmenu guibg=NONE
+      hi PmenuSel guibg=NONE
+      hi StatusLine guibg=NONE
+    ]]
+      end,
+    })
+
+
     vim.cmd([[ colorscheme rose-pine ]])
   end
 }
