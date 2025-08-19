ddca9ea7b8135a702df5e69f270d5445e5a5b818
Joao <devlambertjoao@gmail.com>
2025-08-18 21:53:53 -0300

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,5 @@
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
+  "navarasu/onedark.nvim",
   priority = 1000,
   lazy = false,
   config = function()
@@ -8,37 +7,43 @@ return {
       pattern = "*",
       callback = function()
         vim.cmd [[
-          hi Normal guibg=NONE ctermbg=NONE
-          hi NormalNC guibg=NONE ctermbg=NONE
-          hi NvimTreeNormal guibg=NONE ctermbg=NONE
-          hi SignColumn guibg=NONE ctermbg=NONE
-          hi VertSplit guibg=NONE ctermbg=NONE
-          hi EndOfBuffer guibg=NONE ctermbg=NONE
-          hi NormalFloat guibg=NONE
-          hi FloatBorder guibg=NONE
-          hi TelescopeBorder guibg=NONE
-          hi Pmenu guibg=NONE
-          hi PmenuSel guibg=NONE
-          hi StatusLine guibg=NONE
-          hi StatusLineNC guibg=NONE
+          ""hi Normal guibg=NONE ctermbg=NONE
+          ""hi NormalNC guibg=NONE ctermbg=NONE
+          ""hi NvimTreeNormal guibg=NONE ctermbg=NONE
+          ""hi SignColumn guibg=NONE ctermbg=NONE
+          ""hi VertSplit guibg=NONE ctermbg=NONE
+          ""hi EndOfBuffer guibg=NONE ctermbg=NONE
+          ""hi NormalFloat guibg=NONE
+          ""hi FloatBorder guibg=NONE
+          ""hi TelescopeBorder guibg=NONE
+          ""hi Pmenu guibg=NONE
+          ""hi PmenuSel guibg=NONE
+          ""hi StatusLine guibg=NONE
+          ""hi StatusLineNC guibg=NONE
 
 
-          hi BufferLineFill guibg=NONE
-          hi BufferLineBackground guibg=NONE
-          hi BufferLineSeparator guibg=NONE
-          hi BufferLineSeparatorSelected guibg=NONE
-          hi BufferLineSeparatorVisible guibg=NONE
-          hi BufferLineTab guibg=NONE
-          hi BufferLineTabSelected guibg=NONE
-          hi BufferLineTabClose guibg=NONE
-          hi BufferLineCloseButton guibg=NONE
-          hi BufferLineCloseButtonVisible guibg=NONE
-          hi BufferLineCloseButtonSelected guibg=NONE
+          ""hi BufferLineFill guibg=NONE
+          ""hi BufferLineBackground guibg=NONE
+          ""hi BufferLineSeparator guibg=NONE
+          ""hi BufferLineSeparatorSelected guibg=NONE
+          ""hi BufferLineSeparatorVisible guibg=NONE
+          ""hi BufferLineTab guibg=NONE
+          ""hi BufferLineTabSelected guibg=NONE
+          ""hi BufferLineTabClose guibg=NONE
+          ""hi BufferLineCloseButton guibg=NONE
+          ""hi BufferLineCloseButtonVisible guibg=NONE
+          ""hi BufferLineCloseButtonSelected guibg=NONE
         ]]
       end,
     })
 
+    local theme = require('onedark')
+    theme.setup({
+      style = 'darker'
+    })
+    theme.load()
 
-    vim.cmd([[ colorscheme rose-pine ]])
+
+    -- vim.cmd([[ colorscheme  ]])
   end
 }
