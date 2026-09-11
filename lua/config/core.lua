340981ec1c9da51ae83ece91d89ab4bff92ebeb0
Joao <devlambertjoao@gmail.com>
2026-09-11 18:49:29 -0300

    refreshing files on focus

--- lua/config/core.lua
+++ lua/config/core.lua
@@ -14,7 +14,7 @@ opt.showmode = false
 opt.encoding = 'utf-8'
 
 opt.cursorline = true
-opt.wrap = false
+opt.wrap = true
 
 opt.swapfile = false
 opt.writebackup = false
@@ -24,6 +24,16 @@ opt.updatetime = 100
 opt.autoread = true
 opt.ttyfast = true
 opt.lazyredraw = true
+
+-- Recarrega o buffer se outro processo alterar o arquivo no disco
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI", "TermClose", "TermLeave" }, {
+  group = vim.api.nvim_create_augroup("auto_reload", { clear = true }),
+  callback = function()
+    if vim.fn.getcmdwintype() == "" and vim.bo.buftype == "" then
+      vim.cmd("checktime")
+    end
+  end,
+})
 
 opt.background = 'dark'
 opt.termguicolors = true
